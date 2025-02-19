f02cf1335ce8d8424535ab0a7219bfc47d99db4f
KISHAN1506 <kishanagarwal1506@gmail.com>
2025-02-19 10:57:43 +0530

    Sum of number using Global Parameter

--- 005Recursion3.cpp
+++ 005Recursion3.cpp
@@ -1,2 +1,23 @@
 #include <iostream>
 using namespace std;
+
+// sum of number using recursion
+//  1. Parameter wise and functional way
+
+int sum = 0;
+
+void sumofnum(int n){
+    if (n == 0){
+        return;
+    }
+    sum += n;
+    sumofnum(n-1);
+}
+
+
+int main(){
+    int n;
+    scanf("%d",&n);
+    sumofnum(n);
+    printf("%d\n",sum);
+}
