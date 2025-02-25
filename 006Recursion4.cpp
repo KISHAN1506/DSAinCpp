130fef9f3a67b065b4abefb48d190db150c2d90f
KISHAN1506 <kishanagarwal1506@gmail.com>
2025-02-25 12:49:20 +0530

    Swapping using 2 Pointer

---
+++ 006Recursion4.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+// reversing of array using recursion
+// this can be done by taking two pointers at the first and the last position by making 2 vars
+// or we can just take one pointer "i" and 2nd one "n-i-1" and swap the as i >= n/2 return 
+
+void swapping(int i,int arr[],int n){
+    if(i>=n/2) return;
+    swap(arr[i],arr[n-i-1]);
+    swapping(i+1,arr,n);
+}
+
+
+
+
+// checking if the string is palindrome or not a palindrome asdfdsa == asdfdsa
+
+
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i = 0;i<n;i++) cin>>arr[i];
+    swapping(0,arr,n);
+    for(int i = 0;i<n;i++) cout<<arr[i]<<" ";
+
+
+
+    string s = "madam";
+
+} 
