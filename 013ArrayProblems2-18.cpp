96c035ac4eae2cc1127238a9a139da7491938213
KISHAN1506 <kishanagarwal1506@gmail.com>
2025-03-23 22:50:08 +0530

    Left rotation by 1 element'

---
+++ 013ArrayProblems2-18.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    // Input in arr:
+    for (int i = 0; i < n; i++){
+        cin>>arr[i];
+    }
+    
+
+    // Left rotation of array
+    int temp = arr[0];
+    for(int i = 1;i<n;i++){
+        arr[i-1] = arr[i];
+    }
+    arr[n-1] = temp;
+
+
+    // Printing of array
+    for (int i = 0; i < n; i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
