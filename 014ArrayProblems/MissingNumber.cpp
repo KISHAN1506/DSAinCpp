65e5694f5057262454c4298fa2719d3e13ebe59b
KISHAN1506 <kishanagarwal1506@gmail.com>
2025-05-15 16:01:09 +0530

    Missing number bruteforce approach

---
+++ 014ArrayProblems/MissingNumber.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    int arr[n-1];
+    for(int i = 0;i<n-1;i++){
+        cin>>arr[i];
+    }
+
+    // Missing Number:
+
+    // Bruteforce Approach:
+    for(int i = 1; i <= n;i++){
+        // till n - 1 because 1 number is less that is missing
+        int flag = 0;
+        for(int j = 0;j<n-1;j++){
+            if(arr[j] == i){
+                flag = 1;
+                break;
+            }
+        }
+        if(flag == 0) {
+            printf("%d",i);
+        }
+    }
+    return 0;
+}
