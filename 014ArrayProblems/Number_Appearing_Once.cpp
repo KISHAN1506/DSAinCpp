adf7c22b3e14290a3a0477d6f680717b7ce54f04
KISHAN1506 <kishanagarwal1506@gmail.com>
2025-06-05 13:55:04 +0530

    Number Appearing Once - Half

---
+++ 014ArrayProblems/Number_Appearing_Once.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // Brute Force method
+    int n;
+    cin>>n;
+    int arr[n];
+    for (int i = 0; i < n; i++){
+        cin>>arr[i]
+    }
+    
+    for(int i = 0;i<n;i++){
+        int num = arr[i];
+        int ctr = 0;
+
+        for (int j = 0; j < n; j++){
+            if(arr[j] == )
+        }
+        
+    }
+
+    return 0;
+}
